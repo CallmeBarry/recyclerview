24788204ecdc237b3fd6043a7e73d21b52b69753
Administrator <1024849472@qq.com>
2017-02-21 00:36:31 +0800

    Initial commit

---
+++ app/src/main/java/com/qqdemo/administrator/recyclerview/Dog.java
@@ -0,0 +1,15 @@
+package com.qqdemo.administrator.recyclerview;
+
+/**
+ * Created by yls on 2017/2/20.
+ */
+public class Dog {
+    String name;
+    int imgId;
+
+    public Dog(String name, int imgId) {
+        this.name = name;
+        this.imgId = imgId;
+    }
+
+}
